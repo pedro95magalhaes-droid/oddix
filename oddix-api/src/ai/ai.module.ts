5905a0ac9699cd625ce1fa32d8df96012901e4b4
Pedro Magalhaes <seuemail@gmail.compedro95magalhaes@gmail.com>
2026-05-28 19:55:38 -0300

    register ai controller

--- oddix-api/src/ai/ai.module.ts
+++ oddix-api/src/ai/ai.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AiController } from './ai.controller';
 import { AiService } from './ai.service';
 import { MarketsModule } from '../markets/markets.module';
 import { OddsModule } from '../odds/odds.module';
 
 @Module({
   imports: [MarketsModule, OddsModule],
+  controllers: [AiController],
   providers: [AiService],
   exports: [AiService],
 })
