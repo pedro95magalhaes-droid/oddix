312876c045ed065c2cfe7a1ae59b5c2c8880e71b
Pedro Magalhaes <seuemail@gmail.compedro95magalhaes@gmail.com>
2026-05-28 15:35:32 -0300

    fix jwt secret on render

--- oddix-api/src/auth/strategies/jwt.strategy.ts
+++ oddix-api/src/auth/strategies/jwt.strategy.ts
@@ -8,7 +8,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'oddix_secret',
+      secretOrKey: process.env.JWT_SECRET || 'oddix_secret_2026',
     });
   }
 
