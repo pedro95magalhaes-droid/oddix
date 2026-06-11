0ab162e869ff2063df7893469777c2c15940c84a
Pedro Magalhaes <seuemail@gmail.compedro95magalhaes@gmail.com>
2026-06-11 19:38:13 -0300

    fix cors preflight

--- oddix-api/src/main.ts
+++ oddix-api/src/main.ts
@@ -4,8 +4,18 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
 
-  await app.listen(3001);
+  const port = process.env.PORT || 3001;
+
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`🚀 Oddix rodando na porta ${port}`);
 }
+
 bootstrap();
